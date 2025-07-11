352363e922cbab300056d65fd356e7fca9c57be4
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-07-11 14:53:25 +0100

    Add files via upload

--- list.py
+++ list.py
@@ -3,13 +3,6 @@ l=list(a)
 print("\033c\033[43;30m\n\n")
 ll=[]
 s=""
-for n in l:
-    if n==",":
-        ll=ll+[str(s)]
-        s=""
-    else:
-        s=s+n
-ll=ll+[str(s)]
+print(l)
      
 
-print(ll)
