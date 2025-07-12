f928c565a3e0605a79adea10775213194de8adc8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-07-12 23:19:10 +0100

    Add files via upload

--- list.py
+++ list.py
@@ -1,8 +1,16 @@
-a=str('hello world,hi,there....')
-l=list(a)
-print("\033c\033[43;30m\n\n")
-ll=[]
-s=""
-print(l)
-     
+print("\033c\033[43;30m\ngive me file ? \n")
+a=input()
+f1=open(a,"r")
+b=f1.read()
+f1.close()
+c=b.split("\n")
+d=len(c)
+l=[]
+m=""
+for e in range(d):
+    s=c[d-1-e]
+    m=m+"\n"+s
 
+f1=open(a+".inv","w")
+f1.write(m)
+f1.close()
